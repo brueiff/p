b65c47f33e7e3e0fe6e6207fc2ea97dc83b19fd1
Bruno alves <brunooalvves123@gmail.com>
2021-05-20 21:45:12 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+echo "praticando arrays<br>";
+$Cadastro = array ("maria",30,1.8,true);
+
+//$cadastro = array ("nome"=>"maria da silva<br>");
+
+
+
+//var_dump($cadastro);
+var_dump($Cadastro);
+$tamanho= cont($Cadastro);
+
+for ($i=0; $i < $tamanho; $i++) {
+echo $Cadastro[$i];
+echo"<br>";
+}
